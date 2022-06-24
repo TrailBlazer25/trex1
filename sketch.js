57d08be7166637fc46e6356e6535b8e96ef0a5db
TrailBlazer25 <78034543+TrailBlazer25@users.noreply.github.com>
2022-06-24 19:52:28 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -73,7 +73,7 @@ function setup() {
 function draw() {
   //trex.debug = true;
   background(255);
-  text("Score: "+ score, 500,50);
+  text("Score: "+ score/2, 500,50);
   
   if (gameState===PLAY){
     score = score + Math.round(getFrameRate()/60);
